0e7e0309f8a4304d48f8ec3bac968607de271891
aukcode <oyvind.auk@gmail.com>
2019-10-28 13:17:07 +0100

    add ex for eivind

---
+++ ex_help/eivind.py
@@ -0,0 +1,12 @@
+input1 = 'Temeria'
+input2 = 'IT'
+
+def third_letter(input):
+    letterArray = list(input)
+    if len(letterArray) < 3:
+        return 'q'
+    else:
+        return letterArray[2]
+
+result = third_letter(input2)
+print(result)
