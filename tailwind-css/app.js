42496d4c03799eddb73cb299e0693e6e1b4a659a
aukcode <oyvind.auk@gmail.com>
2019-11-01 23:56:34 +0100

    do a tailwind css tutorial. copy original code

--- tailwind-css/app.js
+++ tailwind-css/app.js
@@ -3,7 +3,6 @@ const app = express();
 const port = 5000;
 
 app.get('/', (req, res) => res.send('Hello World!'));
-// app.get('/tail', express.static(path.join(__dirname, 'dist/index.html')));
-app.use(express.static('dist'))
+app.use(express.static('dist'));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`Listening on port ${port}!`));
