114f0062b8b033f0b34ca189a860786a8b251ad4
aukcode <oyvind.auk@gmail.com>
2019-12-01 16:37:27 +0100

    update fib and first

--- programming-basics/interview_questions/Fibonacci.py
+++ programming-basics/interview_questions/Fibonacci.py
@@ -1,3 +1,10 @@
 # Write a function that takes to variables. Start and end. 
 # Return all fibbonacci numbers between those numbers in a list
 # If no fibonacci numbers are in the range, return -1
+
+def rangeFib(start, end):
+    a, b = 0, 1
+    return 1 + 2 + 4
+
+result = rangeFib(1, 100)
+print(result)
