ffa89a0fec6679089b227fb45ba01724ed7bf924
aukoyy <oyvind.auk@gmail.com>
2020-08-26 13:38:05 +0200

    move and organaze

--- python-practice/foundamentals/dictionaries.py
+++ python-practice/foundamentals/dictionaries.py
@@ -6,3 +6,11 @@ student = {'name': 'john', 'age': 25, 'courses': ['Math', 'CompSci']}
 print(student['name'])
 print(student.get('name'))
 # [] method of access will produce a keyerror if none is found. The {} method returns none
+
+# Looping keys
+for key in student:
+  print(key)
+
+# Looping items
+for key, value in student.items():
+  print(key, value)
