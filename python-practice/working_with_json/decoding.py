1e37eed97a2815cae7bc34983586ba2cb48b7c10
aukoyy <oyvind.auk@gmail.com>
2020-08-26 13:56:42 +0200

    document

--- python-practice/working_with_json/decoding.py
+++ python-practice/working_with_json/decoding.py
@@ -1,5 +1,6 @@
 import json
 
+# To decode a json string format into a python dictionary or list, use json.loads(string)
 load = json.loads('["foo", {"bar":["baz", null, 1.0, 2]}]')
 
 print(type(load))
--- python-practice/working_with_json/encoding.py
+++ python-practice/working_with_json/encoding.py
@@ -3,6 +3,8 @@ import json
 obj = ['foo', {'bar': ('baz', None, 1.0,2)}]
 print(type(obj)) # list
 
+# To encode a python dictionary or list, use json.dumps(object)
+# This will produce a string form in json format
 dump = json.dumps(obj)
 
 print(dump)
