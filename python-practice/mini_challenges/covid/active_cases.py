64fd753b37937d9d1d31f9d7fc51e71803d621ee
aukoyy <oyvind.auk@gmail.com>
2020-08-27 16:42:42 +0200

    finnish json version

--- python-practice/mini_challenges/covid/active_cases.py
+++ python-practice/mini_challenges/covid/active_cases.py
@@ -5,19 +5,23 @@ import datetime
 # API: https://documenter.getpostman.com/view/4876530/SzRxUpPv?version=latest#bf1a26fe-0c42-4b47-8cca-57ce5d9309ac
 
 target_url = 'https://covid19-update-api.herokuapp.com/api/v1/cases/active'
+file_path = '/Users/auk/code/projects/sandbox/python-practice/mini_challenges/covid/active_cases.json'
 data = urlopen(target_url)
 data = data.read().decode()
 data = json.loads(data)
 
-with open('active_cases.json') as f:
+with open(file_path) as f:
   activeDict = json.load(f)
 
 
 date = datetime.date.today()
 week = int(date.strftime("%W"))
-
-
 current_cases = data['activeCases']['total']
+print('### Covid Update ###')
+print('Current cases: ' + '{:,}'.format(current_cases))
+change_this_week = activeDict['weeks'][str(week)]['diff']
+print('Change this week: ' + '{:,}'.format(change_this_week))
+
 def create_new_week(current_cases):
   new_week = {str(week): {
     'high': current_cases,
@@ -26,15 +30,17 @@ def create_new_week(current_cases):
   }}
   activeDict['weeks'].update(new_week)
 
-print(activeDict['weeks']['33'])
-#print(type(activeDict['weeks'][str(week)]['low']))
 
 def update_current_week(current_cases):
-  if current_cases < activeDict['weeks'][str(week)]['low']:
-    activeDict['weeks'][str(week)]['low'] = current_cases
-  if current_cases > activeDict['weeks'][str(week)]['high']:
-    activeDict['weeks'][str(week)]['high'] = current_cases
-  activeDict['weeks'][str(week)]['diff'] = activeDict['weeks'][str(week)]['high'] - activeDict['weeks'][str(week)]['low']
+  low = activeDict['weeks'][str(week)]['low']
+  high = activeDict['weeks'][str(week)]['high']
+  if current_cases < low:
+    activeDict['weeks'][str(week)]['low'], low = current_cases
+  if current_cases > high:
+    activeDict['weeks'][str(week)]['high'], high = current_cases
+  activeDict['weeks'][str(week)]['diff'] = high - low
+
+
 
 if activeDict['totals']['current_week'] < week:
   activeDict['totals']['current_week'] = week
@@ -42,5 +48,5 @@ if activeDict['totals']['current_week'] < week:
 else:
   update_current_week(current_cases)
 
-print(json.dumps(activeDict, indent=2))
-
+with open(file_path, 'w') as f:
+  json.dump(activeDict, f, indent=2)
