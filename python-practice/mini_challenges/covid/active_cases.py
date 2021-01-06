1174bd5aa7a5b5ad46298f40bc36465db7a7a212
aukoyy <oyvind.auk@gmail.com>
2021-01-06 14:03:25 +0100

    create spanish adjustment

--- python-practice/mini_challenges/covid/active_cases.py
+++ python-practice/mini_challenges/covid/active_cases.py
@@ -1,53 +1,50 @@
-from urllib.request import urlopen
 import json
 import datetime
 import requests
 
-# API: https://documenter.getpostman.com/view/4876530/SzRxUpPv?version=latest#bf1a26fe-0c42-4b47-8cca-57ce5d9309ac
 
 date = datetime.date.today()
 week = int(date.strftime("%W"))
 
 url = "https://covid-19-data.p.rapidapi.com/totals"
-file_path = '/Users/auk/code/projects/sandbox/python-practice/mini_challenges/covid/active_cases.json'
-#data = urlopen(target_url)
-#data = data.read().decode()
-#data = json.loads(data)
-#current_cases = data['activeCases']['total']
-
-""" with open(file_path) as f:
-  activeDict = json.load(f)
- """
-
-
 headers = {
     'x-rapidapi-key': "d39c80742emsha4a71413f74e250p132e54jsnf58a627c3630",
     'x-rapidapi-host': "covid-19-data.p.rapidapi.com"
 }
-
 response = requests.request("GET", url, headers=headers)
+data = json.loads(response.text)
 
-print(response.text)
+file_path = '/Users/auk/code/projects/sandbox/python-practice/mini_challenges/covid/active_cases.json'
+
+
+confirmed = 88830659  # data[0]['confirmed']
+recovered = data[0]['recovered']
+deaths = data[0]['deaths']
+active = confirmed - recovered - deaths
+
+
+with open(file_path) as f:
+    activeDict = json.load(f)
+
+lastHigh = activeDict['weeks'][str(week-1)]['high']
+lastLow = activeDict['weeks'][str(week-1)]['low']
+newHigh = lastHigh < active
 
 
 def create_new_week(current_cases):
-    # TODO: add from last week
-    lastHigh = activeDict['weeks'][str(week-1)]['high']
-    lastLow = activeDict['weeks'][str(week-1)]['low']
-
-    if lastHigh < current_cases:  # new high
+    if newHigh:
         new_week = {str(week): {
             'high': current_cases,
             'low': lastHigh,
             'diff': current_cases - lastHigh
         }}
-    else:  # new low
+    else:
         new_week = {str(week): {
             'high': lastLow,
             'low': current_cases,
             'diff': current_cases - lastLow
         }}
-    # activeDict['weeks'].update(new_week)
+    activeDict['weeks'].update(new_week)
 
 
 def update_current_week(current_cases):
@@ -61,20 +58,28 @@ def update_current_week(current_cases):
             activeDict['weeks'][str(week)]['low']
 
 
-""" 
 if activeDict['totals']['current_week'] < week:
-  activeDict['totals']['current_week'] = week
-  create_new_week(current_cases)
+    activeDict['totals']['current_week'] = week
+    create_new_week(active)
 else:
-  update_current_week(current_cases)
+    update_current_week(active)
 
 with open(file_path, 'w') as f:
-  json.dump(activeDict, f, indent=2)
- """
+    json.dump(activeDict, f, indent=2)
 
-print('### Covid Update ###')
-print(date, week)
 
-#print('Current cases: ' + '{:,}'.format(current_cases))
-#change_this_week = activeDict['weeks'][str(week)]['diff']
-#print('Change this week: ' + '{:,}'.format(change_this_week))
+# SPANISH FLUE NUMBERS FOR COMPARASON
+SPANISH_CONFIRMED = 500000000  # word confirmed are used as domain language
+SPANISH_DEATHS = 50000000
+POPULATION_ADJUSTMENT_CONSTANT = 4.19
+SPANISH_CONFIRMED_ADJUSTED = SPANISH_CONFIRMED * POPULATION_ADJUSTMENT_CONSTANT
+SPANISH_DEATHS_ADJUSTED = SPANISH_DEATHS * POPULATION_ADJUSTMENT_CONSTANT
+
+print('### Covid Update ### (<absolute compered to spanish flue>) (adjusted comparason)')
+change_this_week = activeDict['weeks'][str(week)]['diff']
+print(f'Change: {change_this_week:,}')
+print(
+    f'Confirmed: {confirmed:,} ({confirmed/SPANISH_CONFIRMED*100:.2f}%) ({confirmed/SPANISH_CONFIRMED_ADJUSTED*100:.2f}%)')
+print(f'Recovered: {recovered:,}')
+print(f'Deaths: {deaths:,} ({deaths/SPANISH_DEATHS*100:.2f}%) ({deaths/SPANISH_DEATHS_ADJUSTED*100:.2f}%)')
+print(f'Active: {active:,}')
