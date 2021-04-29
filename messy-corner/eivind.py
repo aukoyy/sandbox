ac14081d380d0dbf91984bba0e6b22ccac307b87
aukoyy <22494963+aukoyy@users.noreply.github.com>
2021-04-29 16:23:48 +0200

    remove graphql

--- messy-corner/eivind.py
+++ messy-corner/eivind.py
@@ -1,6 +1,7 @@
 input1 = 'Temeria'
 input2 = 'IT'
 
+
 def third_letter(input):
     letterArray = list(input)
     if len(letterArray) < 3:
@@ -8,5 +9,6 @@ def third_letter(input):
     else:
         return letterArray[2]
 
-result = third_letter(input2)
+
+result = third_letter(input1)
 print(result)
